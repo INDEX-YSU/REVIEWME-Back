80fe36b1f93a8365577d8b55a62a7852c6648ff7
김정욱 <yeoback07@icloud.com>
2026-08-01 12:44:31 +0800

    feat: initial setup

---
+++ src/main/java/club/index_ysu/reviewme/ReviewmeApplication.java
@@ -0,0 +1,13 @@
+package club.index_ysu.reviewme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewmeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/club/index_ysu/reviewme/ReviewmeApplicationTests.java
@@ -0,0 +1,13 @@
+package club.index_ysu.reviewme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
