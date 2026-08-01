a4a7693f8b9a76289c307f746d637919ec26461e
김정욱 <yeoback07@icloud.com>
2026-08-01 13:24:58 +0800

    feat: docker setup

--- src/main/java/club/index_ysu/reviewme/ReviewmeApplication.java
+++ src/main/java/club/index_ysu/reviewme/ReviewmeApplication.java
@@ -2,12 +2,20 @@ package club.index_ysu.reviewme;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ReviewmeApplication {
+ 
+    @RequestMapping("/")
+    public String home() {
+        return "Hello Docker World!";
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReviewmeApplication.class, args);
 	}
-
+ 
 }
